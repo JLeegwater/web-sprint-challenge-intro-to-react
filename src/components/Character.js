e10a97ef16e96b14962d90e86129ecaa616ea0a3
Jesse-Leegwater <Jleegwater@live.com>
2021-07-16 12:35:52 -0700

    think i finished the streach

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const StyledChar = styled.p`
+  background-color: "#181a1b";
+  opacity: 1;
+  color: "#d0ccc5";
+`;
+
+export default function Character({ character }) {
+  //console.log(character);
+  return <StyledChar>{character.name}</StyledChar>;
+}
